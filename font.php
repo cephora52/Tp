b9d7bef7a77a2ea54a2a2167ae046c2f87a618ad
cephora52 <cephora52@gamil.com>
2025-11-04 04:57:09 +0100

    nouvelle modification

--- font.php
+++ font.php
@@ -2,4 +2,12 @@
 $fdate=$_POST['fromdate'];
  $tdate=$_POST['todate'];
 $rtype=$_POST['requesttype'];
+$tdate=$_POST['todate'];
+?>
+<?php
+$userid=$_SESSION['detsuid'];
+$ret=mysqli_query($con,"SELECT ExpenseDate,SUM(ExpenseCost) as totaldaily FROM `tblexpense`  where (ExpenseDate BETWEEN '$fdate' and '$tdate') && (UserId='$userid') group by ExpenseDate");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+
 ?>
